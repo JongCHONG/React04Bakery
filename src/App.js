37732e699850c68ad9dc3371ee537e33ed3c064c
JongCHONG <yonhoa27@hotmail.fr>
2021-11-03 16:30:34 +0100

    03 - State

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import Button from './components/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      activeTab: "add",
+      items: []
+    }
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+  }
+
+  handleButtonClick(e) {
+    this.setState({activeTab: e.target.value})
+    // console.log(e.target.value)
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="container py-3">
-
-        <Button />
+        <Button 
+          handleClick={this.handleButtonClick}
+          isSelected={this.state.activeTab}
+        />
       </div>
     )
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react'
 
 class Button extends Component {
     render() {
+        console.log(this.props)
+        const { handleClick, isSelected } = this.props
         return (
             <div>
-                <button type="button" className="btn btn-outline-primary mx-1">Add</button>
-                <button type="button" className="btn btn-outline-primary mx-1">List</button>
-                <button type="button" className="btn btn-outline-primary mx-1">Pay</button>
+                <button 
+                    type="button" 
+                    className={`btn btn-outline-primary mx-1 ${isSelected === "add" && 'bg-primary text-white'}`}
+                    onClick={handleClick}
+                    value="add"
+                >
+                    Add
+                </button>
+                <button 
+                    type="button" 
+                    className={`btn btn-outline-primary mx-1 ${isSelected === "list" && 'bg-primary text-white'}`}
+                    onClick={handleClick}
+                    value="list"
+                >
+                    List
+                </button>
+                <button 
+                    type="button" 
+                    className={`btn btn-outline-primary mx-1 ${isSelected === "pay" && 'bg-primary text-white'}`}
+                    onClick={handleClick}
+                    value="pay"
+                >
+                    Pay
+                </button>
             </div>
         )
     }
